8505b801e5360cd02444ed3297b7f33f0a2622b7
ozherelkov97 <ozherelkov97@gmail.com>
2022-09-06 21:34:18 +0300

    add sorting by date and fixed delete function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from create_bot import bot
 from keyboards import menu_keyboard, back_keyboard
-from src.utils import new_record, del_record, show_user_records_list
+
+from src.utils import new_record, del_record, show_user_records_list, get_user_records_number
 
 
 @bot.message_handler(commands=['start'])
@@ -12,34 +13,34 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def main_menu(message):
     if message.text == 'Создать':
-        msg_create = bot.send_message(message.chat.id, 'Отправь текст напоминалки', reply_markup=back_keyboard)
-        bot.register_next_step_handler(msg_create, create_record_text)
+        msg = bot.send_message(message.chat.id, 'Отправь текст напоминалки', reply_markup=back_keyboard)
+        bot.register_next_step_handler(msg, create_record_text)
     elif message.text == 'Удалить':
         reply = show_user_records_list(message.from_user.username, mode='delete')
-        msg_create = bot.send_message(message.chat.id, reply, reply_markup=back_keyboard)
-        bot.register_next_step_handler(msg_create, delete_record)
+        msg = bot.send_message(message.chat.id, reply, reply_markup=back_keyboard)
+        bot.register_next_step_handler(msg, delete_record)
     elif message.text == 'Список':
         reply = show_user_records_list(message.from_user.username, mode='list')
-        msg_create = bot.send_message(message.chat.id, reply, reply_markup=menu_keyboard)
-        bot.register_next_step_handler(msg_create, main_menu)
+        msg = bot.send_message(message.chat.id, reply, reply_markup=menu_keyboard)
+        bot.register_next_step_handler(msg, main_menu)
 
 
 @bot.message_handler(content_types=['text'])
 def create_record_text(message):
     if message.text == 'Назад':
-        return_back = bot.send_message(message.chat.id, 'Назад', reply_markup=menu_keyboard)
-        bot.register_next_step_handler(return_back, main_menu)
+        msg = bot.send_message(message.chat.id, 'Назад', reply_markup=menu_keyboard)
+        bot.register_next_step_handler(msg, main_menu)
     else:
         record_text = message.text
-        cron_create = bot.send_message(message.chat.id, 'Как часто напоминать?')
-        bot.register_next_step_handler(cron_create, create_record_cron, record_text)
+        msg = bot.send_message(message.chat.id, 'Как часто напоминать?')
+        bot.register_next_step_handler(msg, create_record_cron, record_text)
 
 
 @bot.message_handler(content_types=['text'])
 def create_record_cron(message, record_text):
     reply = new_record(message, record_text)
-    new_command = bot.send_message(message.chat.id, reply, reply_markup=menu_keyboard)
-    bot.register_next_step_handler(new_command, main_menu)
+    msg = bot.send_message(message.chat.id, reply, reply_markup=menu_keyboard)
+    bot.register_next_step_handler(msg, main_menu)
 
 
 @bot.message_handler(content_types=['text'])
@@ -50,13 +51,17 @@ def delete_record(message):
     else:
         try:
             num = int(message.text)
-            reply = del_record(message, num)
-            new_command = bot.send_message(message.chat.id, reply, reply_markup=menu_keyboard)
-            bot.register_next_step_handler(new_command, main_menu)
+            if num <= 0 or num > get_user_records_number(message.from_user.username):
+                msg = bot.send_message(message.chat.id, 'Нет такого номера', reply_markup=back_keyboard)
+                bot.register_next_step_handler(msg, delete_record)
+            else:
+                reply = del_record(message, num)
+                new_command = bot.send_message(message.chat.id, reply, reply_markup=menu_keyboard)
+                bot.register_next_step_handler(new_command, main_menu)
         except ValueError:
-            msg_create = bot.send_message(message.chat.id, 'Не понял тебя. Введи номер для удаления',
-                                          reply_markup=back_keyboard)
-            bot.register_next_step_handler(msg_create, delete_record)
+            msg = bot.send_message(message.chat.id, 'Не понял тебя. Введи номер для удаления',
+                                   reply_markup=back_keyboard)
+            bot.register_next_step_handler(msg, delete_record)
 
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,20 @@
 import yaml
 import uuid
 from data.cfg import RECORDS_PATH
+import datetime
 
 
-def new_record(message, record_text):
+def new_record(message, text):
     record_id = uuid.uuid4().hex
     cron_text = message.text
     username = message.from_user.username
-    records_num = get_user_records_number(username)
+    create_dttm = datetime.datetime.now().strftime("%Y-%m-%d:%H:%M:%S")
     new_record_dict = {
-        record_id: {'num': records_num + 1,
-                    'username': username,
-                    'record_text': record_text,
-                    'cron_text': cron_text}
+        record_id: {'username': username,
+                    'text': text,
+                    'cron_text': cron_text,
+                    'create_dttm': create_dttm
+                    }
     }
     dump_new_record(new_record_dict)
     records_number = get_user_records_number(username)
@@ -20,18 +22,20 @@ def new_record(message, record_text):
 
 
 def del_record(message, num):
+    real_num = num - 1  # user inputs number from screen (and the list was shifted to start from 1, not 0
     username = message.from_user.username
-    if get_user_records_number == 0:
+    user_records = get_user_records_list(username)
+    if len(user_records) == 0:
         return 'Нечего удалять'
     else:
-        with open(RECORDS_PATH, 'r', encoding='utf-8') as f:
-            records = yaml.safe_load(f)
-        hash_to_del = [h for h, r in records.items() if r['username'] == username and r['num'] == num][0]
-        record_text_to_del = records[hash_to_del]['record_text']
-        cron_text_to_del = records[hash_to_del]['cron_text']
-        del records[hash_to_del]
-        dump_all_records(records)
-        return f'Удалил твою напоминалку №{num} ({record_text_to_del}, {cron_text_to_del})'
+        all_records = read_records()
+        record_to_del = user_records[real_num]
+        hash_to_del = get_hash(record_to_del)
+        text_to_del = record_to_del['text']
+        cron_text_to_del = record_to_del['cron_text']
+        del all_records[hash_to_del]
+        dump_all_records(all_records)
+        return f'Удалил твою напоминалку №{num}: \n{text_to_del} ({cron_text_to_del})'
 
 
 def dump_new_record(new_record_dict):
@@ -46,6 +50,49 @@ def dump_all_records(all_records_dict):
 
 def get_user_records_number(username):
     return len(get_user_records_list(username))
+
+
+def show_user_records_list(username, mode='list'):
+    user_records = get_user_records_list(username)
+    user_records = sort_user_records_list(user_records)
+    if user_records:
+        if mode == 'list':
+            return generate_text_list(user_records)
+        elif mode == 'delete':
+            return generate_text_delete(user_records)
+    else:
+        return 'Ты еще ничего не сохранял'
+
+
+def get_user_records_list(username):
+    records = read_records()
+    if records:
+        user_records = [r for r in records.values() if r['username'] == username]
+        return sort_user_records_list(user_records)
+    else:
+        return []
+
+
+def read_records():
+    with open(RECORDS_PATH, 'r', encoding='utf-8') as f:
+        records = yaml.safe_load(f)
+    return records
+
+
+def sort_user_records_list(records):
+    return sorted(records, key=lambda r: r['create_dttm'])
+
+
+def generate_text_list(records):
+    first_line = "Список твоих записей:\n"
+    lines = "\n".join([f"{i+1}: {r['text']} ({r['cron_text']})" for i, r in enumerate(records)])
+    return first_line + lines
+
+
+def generate_text_delete(records):
+    first_line = 'Введи номер записи, которую ты хочешь удалить:\n'
+    lines = "\n".join([f"{i+1}: {r['text']} ({r['cron_text']})" for i, r in enumerate(records)])
+    return first_line + lines
 
 
 def generate_text_create(num):
@@ -72,39 +119,6 @@ def generate_text_create(num):
         return f'Создал. Теперь у тебя {num} напоминалок'
 
 
-def show_user_records_list(username, mode='list'):
-    records = get_user_records_list(username)
-    if records:
-        if mode == 'list':
-            return generate_text_list(records)
-        elif mode == 'delete':
-            return generate_text_delete(records)
-    else:
-        return 'Ты еще ничего не сохранял'
-
-
-def get_user_records_list(username):
-    with open(RECORDS_PATH, 'r', encoding='utf-8') as f:
-        records = yaml.safe_load(f)
-    if records:
-        return [r for r in records.values() if r['username'] == username]
-    else:
-        return []
-
-
-def generate_text_list(records):
-    first_line = "Список твоих записей:\n"
-    lines = "\n".join([f"{r['num']}: {r['record_text']} ({r['cron_text']})" for r in records])
-    return first_line + lines
-
-
-def generate_text_delete(records):
-    first_line = 'Введи номер записи, которую ты хочешь удалить:\n'
-    lines = "\n".join([f"{r['num']}: {r['record_text']} ({r['cron_text']})" for r in records])
-    return first_line + lines
-
-
-# def get_hash_by_username_num(username, num):
-#     with open(RECORDS_PATH, 'r', encoding='utf-8') as f:
-#         records = yaml.safe_load(f)
-#     return [h for h, r in records.items() if r['username'] == username and r['num'] == num][0]
+def get_hash(record):
+    records = read_records()
+    return [h for h, r in records.items() if r == record][0]
