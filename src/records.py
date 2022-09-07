200ea82afd8eaf0ca09c8107f170886b359b26d2
ozherelkov97 <ozherelkov97@gmail.com>
2022-09-07 23:58:49 +0300

    methods add_record, delete_record, dump_new_record

--- src/records.py
+++ src/records.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, asdict
 from datetime import datetime
+from telebot.types import Message
 
 import yaml
 
@@ -13,14 +14,32 @@ class SingleRecord:
 
 
 class Records:
-    def __init__(self, records_list: list):
+    def __init__(self, records_list: list, path='data/records.yml'):
         self.records = records_list
+        self.path = path
 
     def get_user_records_list(self, username):
         return [rec for rec in self.records if rec.username == username]
 
     def get_user_records_number(self, username):
         return len(self.get_user_records_list(username))
+
+    def add_record(self, username: str, text: str, cron_text: str):
+        new_record = SingleRecord(create_dttm=datetime.now(),
+                                  username=username,
+                                  text=text,
+                                  cron_text=cron_text)
+        self.records.append(new_record)
+        self.dump_new_record(new_record)
+
+    def delete_record(self, record: SingleRecord):
+        index = hash(record)
+        del self.records[index]
+
+    def dump_new_record(self, new_record: SingleRecord):
+        dict_to_dump = asdict(new_record)
+        with open(self.path, 'a', encoding='utf-8') as f:
+            yaml.dump(dict_to_dump, f, allow_unicode=True)
 
 
 reminders_list = []
@@ -29,6 +48,9 @@ with open('data/records.yml', 'r', encoding='utf-8') as f:
 
 for h, r in file_dict.items():
     next_reminder = SingleRecord(**r)
+    print(next_reminder.__hash__())
+    print(next_reminder)
+    print(asdict(next_reminder))
     reminders_list.append(next_reminder)
 
 records = Records(reminders_list)
